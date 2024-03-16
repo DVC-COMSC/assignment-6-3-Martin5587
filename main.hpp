517d72ecdbc5c38149b2a580bc0b3add8ad56165
Martin5587 <mcontreras265@insite.4cd.edu>
2024-03-15 17:48:53 -0700

    HW 6.3

--- main.hpp
+++ main.hpp
@@ -1,4 +1,15 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
 #include <iostream>
 using namespace std;
 
-void getTwoValues(int &, int &);
+void getTwoValues(int &n1, int &n2)
+{
+    std::cout << "Enter the first integer: ";
+    std::cin >> n1;
+
+    std::cout << "Enter the second integer: ";
+    std::cin >> n2;
+}
+
+#endif
